b1db1b9f2c75f9d1e62e48067d037698d79b86b9
Markus Prinz <markus.prinz@nuclearsquid.com>
2011-04-21 14:00:29 +0200

    More emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -111,3 +111,15 @@
         ))
 (el-get 'sync)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inferior-lisp-program "/usr/local/bin/clj"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
