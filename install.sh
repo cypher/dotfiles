7e12cc27f93ed19251729e5100c0f26cae3be0ec
Markus Prinz <markus.prinz@nuclearsquid.com>
2012-02-08 20:54:19 +0100

    Fix installer script

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+set -e
+
+BASE="$(cd "$(dirname "$0")" && pwd)"
+
+
 # Very simple install script
 
-for file in `find ${PWD} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc -not README.txt -not LICENSE -type f`; do
+for file in `find ${BASE} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc -not -name README.txt -not -name LICENSE -type f`; do
     filename=`basename "$file"`
-    echo "ln -s `pwd`/$filename -> ~/.$filename"
-    ln -s `pwd`/$filename ~/.$filename
+    echo "ln -sf $BASE/$filename -> ~/.$filename"
+    ln -sf "$BASE/$filename" ~/.$filename
 done
 
 mkdir -p ~/bin/
-for file in `find ${PWD}/bin/ -maxdepth 1 -not -name '.*'`; do
+for file in "$BASE"/bin/*; do
     filename=`basename "$file"`
-    echo "ln -s `pwd`/$filename -> ~/bin/$filename"
-    ln -s `pwd`/$filename ~/bin/$filename
+    echo "ln -sf $BASE/$filename -> ~/bin/$filename"
+    ln -sf "$BASE/bin/$filename" ~/bin/$filename
 done
 
 # Special case: Make bin/git-reup a link to git-up
-if [[-e ~/bin/git-up ]]; then
-    ln -s ~/bin/git-up ~/bin/git-reup
+if [[ -e ~/bin/git-up ]]; then
+    ln -sf ~/bin/git-up ~/bin/git-reup
 fi
