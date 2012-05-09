a0fb3b146079d02144ae3ab8a08636bdb385e6fe
Markus Prinz <markus.prinz@nuclearsquid.com>
2012-05-09 23:00:36 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ BASE="$(cd "$(dirname "$0")" && pwd)"
 
 # Very simple install script
 
-for file in `find ${BASE} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc -not -name README.txt -not -name LICENSE -type f`; do
+for file in `find ${BASE} -maxdepth 1 -not -name '.*' -not -name install.sh -not -name bin -not -name tasks -not -name etc -not -name README.txt -not -name LICENSE`; do
     filename=`basename "$file"`
     echo "ln -sf $BASE/$filename -> ~/.$filename"
     ln -sf "$BASE/$filename" ~/.$filename
