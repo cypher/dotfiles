f8b4ea6139fe11602e1ec5cba1d82d9b9d2303aa
Markus Wein <mtw@geolad.com>
2017-06-21 12:01:56 +0200

    Add bookmarklet to easily save to Ulysses

---
+++ Misc/Save to Ulysses Bookmarklet.js
@@ -0,0 +1,2 @@
+// To get sheet id: Alt-Right Click on sheet, "Copy Callback Identifier"
+javascript:(function(){window.location="ulysses://x-callback-url/insert?id=<Callback ID>&text="+encodeURIComponent("["+document.title+"]("+window.location+")\n");}());
